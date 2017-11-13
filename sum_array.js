807b0dc6e015cae9369311ac058a08856e726b55
hacktiv8 <hacktiv8@Sabtas-Mac-mini.local>
2017-11-13 22:07:14 +0700

    done

--- sum_array.js
+++ sum_array.js
@@ -1,5 +1,16 @@
 function sum_array(arr) {
-  //your code here
+var res = 0;
+  if(arr[1] > arr[0]){
+    for (var i = 0; i < arr[1]; i++) {
+      res += i;
+    }
+    return res + arr[1];
+  } else {
+    for (var i = 0; i < arr[0]; i++) {
+      res += i;
+    }
+    return res + arr[0];
+  }
 }
 
 
@@ -7,7 +18,7 @@ function sum_array(arr) {
 console.log('Testing\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
