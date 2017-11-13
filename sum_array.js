679828f79429b203d34cd44c417c2d36fafa9b41
Haniiful Wahib <wahib@Haniifuls-MacBook-Pro.local>
2017-11-13 20:24:20 +0700

    initial submit by Wahib

--- sum_array.js
+++ sum_array.js
@@ -1,5 +1,23 @@
 function sum_array(arr) {
   //your code here
+  //console.log(arr[0]);
+  if (Math.abs(arr[1]-arr[0]) <= 1){
+    if (arr[0] === arr[1] ){
+      return arr[0];
+    } else {
+      return arr[0]+arr[1];
+    }
+  } else {
+    var small = arr[0];
+    var big = arr[1];
+    //console.log('>>', big , '+', small)
+    if (small < big){
+      //console.log(big);
+      return small + big + sum_array([small+1,big-1])
+    } else {
+      return small+ big + sum_array([big+1,small-1])
+    }
+  }
 }
 
 
@@ -7,7 +25,7 @@ function sum_array(arr) {
 console.log('Testing\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
