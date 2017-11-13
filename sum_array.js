d454832b56e9e4db1e4b15966d76ed070eb9dfc9
alangmahendra <alangmahemdra@gmail.com>
2017-11-13 21:18:20 +0700

    commit

--- sum_array.js
+++ sum_array.js
@@ -1,13 +1,24 @@
 function sum_array(arr) {
-  //your code here
+  if(arr[0]<arr[1]){
+    let result = 0
+      for(let i = 0;i<arr[1];i++){
+    result+=i
+   }
+  return result+arr[1]
+  }else{
+    let result = 1
+      for(let i = arr[0];i>arr[1];i--){
+    result+=i
+   }
+  return result
+  }
 }
-
-
+// console.log(sum_array([1,5]));
 //driver code
 console.log('Testing\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
