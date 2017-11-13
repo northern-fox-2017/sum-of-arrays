aa77903139c9d82f24fdf3faff88734b2a894cab
idabagusangga <angga.idabagus@gmail.com>
2017-11-13 16:12:33 +0700

    selesai idabagusangga

--- sum_array.js
+++ sum_array.js
@@ -1,13 +1,37 @@
 function sum_array(arr) {
   //your code here
+  if(arr[1] < arr[0]){
+    var stopper = arr[0]
+    var start = arr[1]
+  }
+  else{
+    var stopper = arr[1]
+    var start = arr[0]
+  }
+
+
+  let total = 0
+
+  if (stopper == 1){
+    return total + 1
+  }
+  else{
+    total += stopper;
+    let numRecursive = stopper - 1
+    let arrHasil = []
+    arrHasil.push(start)
+    arrHasil.push(numRecursive)
+    // console.log(arrHasil)
+    return sum_array(arrHasil) + total
+  }
 }
 
 
 //driver code
 console.log('Testing\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('    input   |  expected  | actual')
+console.log('————————————|————————————|————————')
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
