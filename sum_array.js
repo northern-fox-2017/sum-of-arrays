a6352cd24a1339f96e4b3a814e9cf6e184b14de8
name": null <email": null>
2017-11-13 19:46:47 +0700

    Insert function

--- sum_array.js
+++ sum_array.js
@@ -1,5 +1,10 @@
 function sum_array(arr) {
-  //your code here
+  sum = arr[0]+arr[1];
+  arr.sort();
+  for (var i = arr[0]+1; i < arr[1]; i++) {
+    sum+=i;
+  }
+  return sum;
 }
 
 
@@ -7,7 +12,7 @@ function sum_array(arr) {
 console.log('Testing\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
