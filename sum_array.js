b1d16e788a688ec7683bc1e46b9b966f4462f5ef
Heraldo Yusron <heraldoyusrontris@gmail.com>
2017-11-16 15:57:19 +0700

    SumOfArray heraldo

--- sum_array.js
+++ sum_array.js
@@ -1,5 +1,18 @@
 function sum_array(arr) {
   //your code here
+  var tampung = 0;
+  var arr1 = Number(arr[0]);
+  var arr2 = Number(arr[1]);
+  if(arr1 < arr2){
+    for(var i=arr1+1; i<arr2; i++){
+      tampung += i;
+    }
+  }else if(arr1 > arr2){
+    for(var j=arr2+1; j<arr1; j++){
+      tampung += j;
+    }
+  }
+  return arr1 + tampung + arr2;
 }
 
 
@@ -7,7 +20,7 @@ function sum_array(arr) {
 console.log('Testing\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
-console.log('[1,5]       | 15         | ', sum_array([1,5])
-console.log('[1,100]     | 5050       | ', sum_array([1,100])
-console.log('[5,1]       | 15         | ', sum_array([5,1])
-console.log('[100,1]     | 5050       | ', sum_array([100,1])
+console.log('[1,5]       | 15         | ', sum_array([1,5]))
+console.log('[1,100]     | 5050       | ', sum_array([1,100]))
+console.log('[5,1]       | 15         | ', sum_array([5,1]))
+console.log('[100,1]     | 5050       | ', sum_array([100,1]))
